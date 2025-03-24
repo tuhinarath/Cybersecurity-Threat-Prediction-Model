cca737271d1c5ae22be77bbf703b385c07ed01b7
Tuhina Rath <tuhinarath@Tuhinas-MacBook-Air.local>
2025-03-25 04:07:57 +0530

    Improved dataset and tested Logistic Regression

--- ids_model.py
+++ ids_model.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 # Creating a simple dataset (normal = 0, attack = 1)
 data = {
-    'traffic_speed': [100, 120, 300, 90, 400, 110, 500, 80, 150, 350],
-    'packet_size': [500, 550, 1500, 480, 2000, 600, 2500, 450, 700, 1800],
-    'is_attack': [0, 0, 1, 0, 1, 0, 1, 0, 0, 1]  # 0 = normal, 1 = attack
+    'traffic_speed': [100, 120, 300, 90, 400, 110, 500, 80, 150, 350, 200, 250, 450],
+    'packet_size': [500, 550, 1500, 480, 2000, 600, 2500, 450, 700, 1800, 1200, 1600, 2200],
+    'is_attack': [0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1]  # 0 = normal, 1 = attack
 }
+
 
 # Convert to DataFrame
 df = pd.DataFrame(data)
@@ -18,8 +19,8 @@ X = df[['traffic_speed', 'packet_size']]
 y = df['is_attack']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Training a simple Random Forest model
-model = RandomForestClassifier()
+
+model = LogisticRegression()
 model.fit(X_train, y_train)
 
 # Predicting on test data
